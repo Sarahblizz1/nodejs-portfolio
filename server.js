5b0b820eba1ff17bd87315fd2581205f7c3081ca
Sarahblizz1 <blizzsarah@gmail.com>
2022-10-28 12:08:14 +0100

    my commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+const server = http.createServer((request, response) => {
+    if(request.url === '/'){
+    let filePath = path.join(__dirname, 'portfolio', 'home.html');
+    fs.readFile(filePath, 'utf8', (err, content) => {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(content)
+        })
+}
+
+if(request.url === '/about.html'){
+    let filePath = path.join(__dirname, 'portfolio', 'about.html');
+    fs.readFile(filePath, 'utf8', (err, content) => {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(content)
+        })
+    }
+    
+    if(request.url === '/contact.html'){
+        let filePath = path.join(__dirname, 'portfolio', 'contact.html');
+        fs.readFile(filePath, 'utf8', (err, content) => {
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.end(content)
+            })
+    }
+})
+
+const port = 7500
+
+server.listen(port, () => {
+    console.log(`succesfuly logged into ${port}`);
+});
